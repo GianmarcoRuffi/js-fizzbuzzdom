7277477405c9989f40517a3e9883412062963c04
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-03-29 10:58:11 +0200

    Correzione con l'esempio in classe

--- script.js
+++ script.js
@@ -21,21 +21,61 @@ let appended = document.createElement("div");
 
 // Assegnazione classe
 div.className = "square";
+appended.setAttribute ("class", "row")
 
 // Contenuto
 
 div.innerHTML = "FizzBuzz";
 document.body.append(div);
 
+
+
+
+<div class="row">
+          <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+            <div class="square">Roba</div>
+          </div>
+        </div>
+
+
 */
+
+//Create elements
+
+const output = document.getElementById("output");
+const row = document.createElement("div");
+row.setAttribute("class", "row");
 
 //Condizionali
 
 for (let i = 1; i <= maxNumber; i++) {
-  {
-    if (i % 15 == 0) document.write("FizzBuzz", "<br>");
-    else if (i % 3 == 0) document.write("Fizz", "<br>");
-    else if (i % 5 == 0) document.write("Buzz", "<br>");
-    else document.write(i, "<br>");
+  //Creo colonna con classi
+
+  const col = document.createElement("div");
+  col.setAttribute("class", " g-3 col-12 col-sm-6 col-md-4 col-lg-2");
+  //
+
+  // Creo div box con classi
+  const box = document.createElement("div");
+  box.setAttribute("class", "square");
+  let value;
+  //
+
+  if (i % 15 == 0) {
+    console.log("fizzbuzz");
+    value = "fizzbuzz";
+  } else if (i % 3 == 0) {
+    value = "fizz";
+  } else if (i % 5 == 0) {
+    value = "buzz";
+  } else {
+    box.innerHTML = i;
+    value = i;
   }
+  box.innerHTML = value;
+  box.classList.add(value);
+  col.append(box);
+  row.append(col);
 }
+
+output.append(row);
